5a3658a643ef95e954c319575cd21603f46dadc1
behrad_norouzieh <behrad.norouzieh@snappgroup.net>
2024-12-25 22:25:11 +0330

    embedding.go fixed.

--- echo/pkg/embedding.go
+++ echo/pkg/embedding.go
@@ -2,7 +2,5 @@ package pkg
 
 import "embed"
 
-//go:embed services/base.go
-//go:embed services/authentication/definition.go
-//go:embed services/carpay/definition.go
+//go:embed services/*
 var EmbeddedFiles embed.FS
