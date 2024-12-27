86381d58c1d363f24d77abba224561efe277fe32
behrad_norouzieh <behrad.norouzieh@snappgroup.net>
2024-12-27 21:23:44 +0330

    JSON request handling moved to gateway business handler.

--- gateway/api/business/handlers.go
+++ gateway/api/business/handlers.go
@@ -1,9 +1,11 @@
 package business
 
 import (
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"modular_chassis/gateway/internal/service/swagger"
 	"modular_chassis/mediator/pkg/api"
+	"reflect"
 )
 
 var swaggerJSONDoc = func(c *fiber.Ctx) error {
@@ -22,11 +24,24 @@ var generalAPI = func(c *fiber.Ctx) error {
 	domain := c.Params("domain")
 	service := c.Params("service")
 
-	res, err := api.GetMediatorAPI().JSONRoute(c.Context(), domain, service, string(c.Body()))
+	reqModel, _ := api.GetMediatorAPI().GetServiceModels(domain, service)
+
+	request := reflect.New(reqModel).Interface()
+	err := json.Unmarshal(c.Body(), request)
+	if err != nil {
+		return err
+	}
+
+	response, err := api.GetMediatorAPI().Route(c.Context(), domain, service, request)
+	if err != nil {
+		return err
+	}
+
+	marshal, err := json.Marshal(response)
 	if err != nil {
 		return err
 	}
 
 	c.Set("Content-Type", "application/json")
-	return c.SendString(res)
+	return c.SendString(string(marshal))
 }
--- mediator/pkg/api/mediator.go
+++ mediator/pkg/api/mediator.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"encoding/json"
 	"modular_chassis/mediator/internal/service"
 	"reflect"
 	"sync"
@@ -33,25 +32,8 @@ func (m *mediatorAPI) Route(ctx context.Context, serviceType, method string, req
 	return response, nil
 }
 
-func (m *mediatorAPI) JSONRoute(ctx context.Context, serviceType, method string, raw string) (string, error) {
-	reqModel, _ := service.GetRegistry().GetServiceModels(serviceType, method)
-
-	request := reflect.New(reqModel).Interface()
-	err := json.Unmarshal([]byte(raw), request)
-	if err != nil {
-		return "", err
-	}
-
-	response, err := service.GetRouter().HandleRequest(ctx, serviceType, method, request)
-	if err != nil {
-		return "", err
-	}
-
-	marshal, err := json.Marshal(response)
-	if err != nil {
-		return "", err
-	}
-	return string(marshal), nil
+func (m *mediatorAPI) GetServiceModels(serviceType, method string) (request, response reflect.Type) {
+	return service.GetRegistry().GetServiceModels(serviceType, method)
 }
 
 func (m *mediatorAPI) RegisterServiceFunc(serviceImpl interface{}) {
